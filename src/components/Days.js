58197469a6d6f601550e2282d6a045e65abc0998
StenAntonov <sten.antonov@gmail.com>
2022-09-15 00:02:41 +0300

    clean db

--- src/components/Days.js
+++ src/components/Days.js
@@ -1,10 +1,9 @@
 import { useEffect } from "react";
 import DayTemp from "./DayTemp";
 
-const Days = ({daysInMonth}) => {
+const Days = ({ daysInMonth }) => {
   const todaysDate = new Date().getDate();
-  // const daysInMonth = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
-  
+
   useEffect(() => {
     const presentDayBox = document.getElementById(todaysDate);
     presentDayBox.classList.add("today");
@@ -31,7 +30,7 @@ const Days = ({daysInMonth}) => {
         </section>
 
         {daysInMonth.map(day => <DayTemp id={day} key={day} />)}
-        
+
         <section className="day-next">
           <li className="day"><br></br></li>
           <li className="day-events"></li>
